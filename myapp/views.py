37619ebcb433b13d54734ac09ede433a35dbddb8
khalid-jamal <jamalkhalid544@gmail.com>
2023-09-24 10:48:54 +0530

    some changes

--- myapp/views.py
+++ myapp/views.py
@@ -3,12 +3,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello khalid jamal")
+    return HttpResponse("Hello khalid jamal, wellcome to my website.")
 
 
 class Test(APIView):
 
     def get(self,request):
         return Response({
-            'name':'irfan ali'
+            'name':'irfan ali',
+            'age':25
         })
