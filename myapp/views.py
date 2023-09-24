f84d6e5733fce992c38b88656fbdd88a01bcea40
khalid-jamal <jamalkhalid544@gmail.com>
2023-09-24 10:26:10 +0530

    add name

--- myapp/views.py
+++ myapp/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello world")
+    return HttpResponse("Hello khalid jamal")
 
 
 class Test(APIView):
